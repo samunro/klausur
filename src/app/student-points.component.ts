887847af463f1e2033abaf0c83bbe544ba673da7
samunro <scott.munro@yahoo.com>
2021-03-05 06:50:41 +0100

    First version that I sent to Sascha for some feedback.

--- src/app/student-points.component.ts
+++ src/app/student-points.component.ts
@@ -12,14 +12,17 @@ export class StudentPointsComponent {
     console.dir(skills[1].length);
 
     for (const area of criteria.areas) {
+      this.areaWeights.push({areaId: area.id, weight: 100/criteria.areas.length})
+
       for(const skill of area.skills){
-        this.skillGrades.push({ grade: 0, skillId: skill.id });
+        this.skillGrades.push({ grade: 0, skillId: skill.id, pointRangeIds: [] });
       }
     }
   }
 
   criteria = criteria;
   skillGrades: SkillGrade[] = [];
+  areaWeights: AreaWeight[] = [];
 
   getGrade(skillId: number){
     return this.getSkillGrade(skillId).grade;
@@ -33,9 +36,84 @@ export class StudentPointsComponent {
   private getSkillGrade(skillId: number){
     return this.skillGrades.find(x => x.skillId === skillId);
   }
+
+  isCriteriaChecked(pointRangeId: number, skillId: number){
+    const skill = this.getSkillGrade(skillId);
+
+    return skill.pointRangeIds.includes(pointRangeId);
+  }
+
+  checkCriteria(isChecked: boolean, pointRangeId: number, skillId: number){
+    const skill = this.getSkillGrade(skillId);
+
+    if(!isChecked) {
+      skill.pointRangeIds = skill.pointRangeIds.filter(x => x !== pointRangeId);
+    }
+    else
+    {
+      if(!skill.pointRangeIds.includes(pointRangeId)){
+        skill.pointRangeIds.push(pointRangeId);
+      }
+    }
+
+    this.setGrade(this.calculatedGrade(skillId), skillId);
+  }
+
+  calculatedGrade(skillId: number){
+    const skill = this.getSkillGrade(skillId);
+
+    if(!skill.pointRangeIds.length) return 0;
+
+    const averagePointGrades = this.criteria.pointRanges.filter(x => skill.pointRangeIds.includes(x.id)).map(x => (x.minimum + x.maximum)/2)
+
+    return Math.round(this.average(averagePointGrades));
+  }
+
+  private average(elements: number[]){
+    const sum = this.sum(elements);
+
+    return sum / elements.length;
+  }
+
+  private sum(elements: number[]){
+    return elements.reduce((a, b) => a + b, 0);
+  }
+
+  getGradeForArea(areaId: number){
+    const area = this.criteria.areas.find(x => x.id === areaId);
+
+    return this.sum(area.skills.map(x => this.getGrade(x.id)));
+  }
+
+  getTotalAreaWeights(){
+    return this.sum(this.areaWeights.map(x => x.weight));
+  }
+  getAreaWeight(areaId: number) {
+    return this.getAreaWeightObject(areaId).weight;
+  }
+
+  setAreaWeight(weight: number, areaId: number){
+    const areaWeight = this.getAreaWeightObject(areaId);
+
+    areaWeight.weight = weight;
+  }
+
+  private getAreaWeightObject(areaId: number){
+    return this.areaWeights.find(x => x.areaId === areaId);
+  }
+
+  getTotalGrade(){
+    return this.sum(this.criteria.areas.map(x => this.getGradeForArea(x.id) * this.getAreaWeight(x.id)/100));
+  }
 }
 
 class SkillGrade {
   skillId: number;
   grade: number;
+  pointRangeIds: number[];
+}
+
+class AreaWeight {
+  areaId: number;
+  weight: number;
 }
