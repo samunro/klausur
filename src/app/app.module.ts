93ea96367245207a9df55e2c8d7984883a151d38
samunro <scott.munro@yahoo.com>
2021-03-07 18:06:34 +0100

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Inject } from '@angular/core';
+import { BrowserModule, Meta, Title } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { StudentPointsComponent } from './student-points.component';
 import { HelloComponent } from './hello.component';
+import { DOCUMENT } from "@angular/common";
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent, StudentPointsComponent ],
-  bootstrap:    [ StudentPointsComponent ]
+  bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(meta: Meta, title: Title, @Inject(DOCUMENT) document: Document){
+    meta.updateTag({charset: "utf-8"});
+    title.setTitle("Klausur");
+    document.documentElement.lang = "de";
+  }
+}
