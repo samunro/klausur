b75edcc5128d978659aeb68915bbc7f080ebf06b
samunro <scott.munro@yahoo.com>
2021-03-13 16:05:19 +0100

    Hid file inputs to make them look like buttons.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { NgModule, Inject } from '@angular/core';
-import { BrowserModule, Meta, Title } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule, Inject } from "@angular/core";
+import { BrowserModule, Meta, Title } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { StudentPointsComponent } from './student-points.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { StudentPointsComponent } from "./student-points.component";
+import { HelloComponent } from "./hello.component";
 import { DOCUMENT } from "@angular/common";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, StudentPointsComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, StudentPointsComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { 
-  constructor(meta: Meta, title: Title, @Inject(DOCUMENT) document: Document){
-    meta.updateTag({charset: "utf-8"}, "charset=utf-8");
+export class AppModule {
+  constructor(meta: Meta, title: Title, @Inject(DOCUMENT) document: Document) {
+    meta.updateTag({ charset: "utf-8" }, "charset=utf-8");
     title.setTitle("Klausur");
     document.documentElement.lang = "de";
   }
