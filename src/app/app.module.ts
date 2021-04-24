0a912984027878fbb1a2b49aad49961a20ffd6f2
samunro <scott.munro@yahoo.com>
2021-04-24 21:07:14 +0200

    Registered the de-DE locale to ensure that numbers are formatted correctly.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,35 @@
-import { NgModule, Inject } from "@angular/core";
+import { NgModule, Inject, LOCALE_ID } from "@angular/core";
+import localeDe from '@angular/common/locales/de';
+import localeDeExtra from '@angular/common/locales/extra/de';
 import { BrowserModule, Meta, Title } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { StudentPointsComponent } from "./student-points.component";
 import { HelloComponent } from "./hello.component";
-import { DOCUMENT } from "@angular/common";
-import { NgxBootstrapIconsModule, printer, pencilSquare, fileEarmarkArrowDown, fileEarmarkArrowUp } from "ngx-bootstrap-icons";
+import { DOCUMENT, registerLocaleData } from "@angular/common";
+import {
+  NgxBootstrapIconsModule,
+  printer,
+  pencilSquare,
+  fileEarmarkArrowDown,
+  fileEarmarkArrowUp
+} from "ngx-bootstrap-icons";
 
-const icons = { printer, pencilSquare, fileEarmarkArrowDown, fileEarmarkArrowUp };
+registerLocaleData(localeDe, 'de-DE', localeDeExtra)
+
+const icons = {
+  printer,
+  pencilSquare,
+  fileEarmarkArrowDown,
+  fileEarmarkArrowUp
+};
 
 @NgModule({
   imports: [BrowserModule, FormsModule, NgxBootstrapIconsModule.pick(icons)],
   declarations: [AppComponent, HelloComponent, StudentPointsComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent]  ,
+  providers: [{ provide: LOCALE_ID, useValue: "de-DE" }]
 })
 export class AppModule {
   constructor(meta: Meta, title: Title, @Inject(DOCUMENT) document: Document) {
